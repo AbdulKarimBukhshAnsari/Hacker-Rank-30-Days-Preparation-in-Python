9ccab3a771d5e7c134b98978fa2663165c638e1a
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-04-12 18:15:07 +0500

    Problem 4 has been solved

---
+++ problem 4 sparse arrays/problem_4.py
@@ -0,0 +1,37 @@
+import os
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    result = []
+    for i in queries:
+        count_query = strings.count(i)
+        result.append(count_query)
+    print(result)    
+
+if __name__ == '__main__':
+  
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
