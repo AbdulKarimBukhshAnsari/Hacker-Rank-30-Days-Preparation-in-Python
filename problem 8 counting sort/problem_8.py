c2d1fc69da2e99cbc9a46a1192080e995f19d80c
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-04-14 08:04:49 +0500

    Problem 8 has been solved

---
+++ problem 8 counting sort/problem_8.py
@@ -0,0 +1,24 @@
+
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    result_array = [0 for _ in range(100)]
+    for i in arr:
+        result_array[i+1]+=1
+    print(result_array) 
+
+if __name__ == '__main__':
+    
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+   
